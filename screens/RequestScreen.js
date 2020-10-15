3e0c26a4fad50cf3c3ea48a1ee5420aef25ece71
sohamgandhi2007 <63003386+sohamgandhi2007@users.noreply.github.com>
2020-10-15 11:58:58 +0530

    Add files via upload

---
+++ screens/RequestScreen.js
@@ -0,0 +1,122 @@
+import React,{Component} from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  KeyboardAvoidingView,
+  StyleSheet,
+  TouchableOpacity,
+  Alert} from 'react-native';
+import db from '../config';
+import firebase from 'firebase';
+import MyHeader from '../components/MyHeader'
+
+export default class RequestScreen extends Component{
+  constructor(){
+    super();
+    this.state ={
+      userId : firebase.auth().currentUser.email,
+      bookName:"",
+      reasonToRequest:""
+    }
+  }
+
+  createUniqueId(){
+    return Math.random().toString(36).substring(7);
+  }
+
+
+
+  addRequest =(ThingName,reasonToRequest)=>{
+    var userId = this.state.userId
+    var randomRequestId = this.createUniqueId()
+    db.collection('requested_books').add({
+        "user_id": userId,
+        "thing_name":thingName,
+        "reason_to_request":reasonToRequest,
+        "request_id"  : randomRequestId,
+    })
+
+    this.setState({
+        thingName :'',
+        reasonToRequest : ''
+    })
+
+    return Alert.alert("Thing Requested Successfully")
+  }
+
+
+  render(){
+    return(
+        <View style={{flex:1}}>
+          <MyHeader title="Request Thing"/>
+            <KeyboardAvoidingView style={styles.keyBoardStyle}>
+              <TextInput
+                style ={styles.formTextInput}
+                placeholder={"enter thing name"}
+                onChangeText={(text)=>{
+                    this.setState({
+                        thingName:text
+                    })
+                }}
+                value={this.state.thingName}
+              />
+              <TextInput
+                style ={[styles.formTextInput,{height:300}]}
+                multiline
+                numberOfLines ={8}
+                placeholder={"Why do you need the thing"}
+                onChangeText ={(text)=>{
+                    this.setState({
+                        reasonToRequest:text
+                    })
+                }}
+                value ={this.state.reasonToRequest}
+              />
+              <TouchableOpacity
+                style={styles.button}
+                onPress={()=>{this.addRequest(this.state.thingName,this.state.reasonToRequest)}}
+                >
+                <Text>Request</Text>
+              </TouchableOpacity>
+            </KeyboardAvoidingView>
+        </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  keyBoardStyle : {
+    flex:1,
+    alignItems:'center',
+    justifyContent:'center'
+  },
+  formTextInput:{
+    width:"75%",
+    height:35,
+    alignSelf:'center',
+    borderColor:'#ffab91',
+    borderRadius:10,
+    borderWidth:1,
+    marginTop:20,
+    padding:10,
+  },
+  button:{
+    width:"75%",
+    height:50,
+    justifyContent:'center',
+    alignItems:'center',
+    borderRadius:10,
+    backgroundColor:"#ff5722",
+    shadowColor: "#000",
+    shadowOffset: {
+       width: 0,
+       height: 8,
+    },
+    shadowOpacity: 0.44,
+    shadowRadius: 10.32,
+    elevation: 16,
+    marginTop:20
+    },
+  }
+)
